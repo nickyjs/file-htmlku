58a85bfb86feb2c0bb8a59e66048ad8a6daee4dc
Nicky <nickyjulyas03@gmail.com>
2022-09-27 21:24:30 +0700

    upload file baru

---
+++ coba.js
@@ -0,0 +1,64 @@
+// let panjang = 10;
+// let lebar = 7;
+
+// let luas = panjang * lebar;
+
+// console.log(luas);
+
+// for (let i = 1; i <=10; i++){
+//   if (i == 6){
+//     console.log(i, "yess ketemu")
+//   }else{
+//     console.log(i)
+//   }
+// }
+//for = ketika loopnya sudah diketahui berapa banyak akan diulang
+//while = ketika kondisinya ditanyakan dulu
+
+//do while
+// do {
+//   //eksekusi
+// }while(/*conditional*/)
+
+//soal
+// let sayHello = 'Hello';
+// const sayGoodbye = 'Goodbye';
+
+// const speakIndonesia = () =>{
+//   sayHello = 'Hai!';
+//   console.log(sayHello);
+//   console.log(sayGoodbye);
+// };
+
+// speakIndonesia();
+
+
+
+// const address = () => {
+//   const county= 'Indonesia';
+//   const province = 'DKI Jakarta';
+//   const city = 'Jakarta Selatan';
+
+// };
+
+// console.log('Next stop: ${city}');
+
+//soal
+// function sum(a = 4, b){
+//   return a + b
+// }
+
+// num1 = 5
+// num2 = 10
+
+// console.log(sum(num1,num2))
+
+//soal
+// const str1 = 'nicky';
+// console.log(str1.toUpperCase());
+
+//soal
+// const str1 = 'hello';
+// const str2 = 'world';
+
+// console.timeLog(str1.concat(' ', str2))
